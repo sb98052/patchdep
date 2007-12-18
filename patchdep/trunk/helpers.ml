2357af9a1a4e728df402615e2cad42f7f6a5b975
sapanb <sapanb@8c455092-636d-4788-adf5-e71def0336e8>
2007-12-18 17:44:41 +0000

    Support for patchlevels

--- patchdep/trunk/helpers.ml
+++ patchdep/trunk/helpers.ml
@@ -1,22 +1,30 @@
 open Printf
 
-let remove_parent_directory str =
+let remove_parent_directory patchlevel str =
   let len = String.length str in
-  let i = try String.index str '/' with e->
-      printf "Bad filename: %s\n" str;raise e
+  let rec prune idx level =
+    if (level=0) then
+      String.sub str idx (len-idx-1) 
+    else
+      let i = try String.index_from str idx '/' with e->
+        begin
+        printf "Bad filename: %s\n" str;raise e
+        end
+      in
+        prune (i+1) (level-1)
   in
-    String.sub str (i+1) (len-i-2)
+    prune 0 patchlevel
 
 let read_list_from_file f =
   if (f="") then []
   else
-  let f_file = try open_in f with e -> printf "Could not open file
-                                         %s\n" f;flush Pervasives.stdout;raise e
-  in
-  let rec read_file cur_list =
-    let next_line = try Some(input_line f_file) with _ -> None in
-      match next_line with
-        | Some(inp_line) -> read_file (cur_list@[inp_line])
-        | None -> close_in f_file;cur_list
-  in
-    read_file []
+    let f_file = try open_in f with e -> printf "Could not open file
+                                           %s\n" f;flush Pervasives.stdout;raise e
+    in
+    let rec read_file cur_list =
+      let next_line = try Some(input_line f_file) with _ -> None in
+        match next_line with
+          | Some(inp_line) -> read_file (cur_list@[inp_line])
+          | None -> close_in f_file;cur_list
+    in
+      read_file []
